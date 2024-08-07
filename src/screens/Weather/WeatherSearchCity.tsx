53b428d98dff13f51d485833190f82291aa398d4
Abinash Karmakar <codeAbinash@gmail.com>
2024-08-07 11:19:26 +0530

    refactor: Improve WeatherSearchCity component structure and styling

--- src/screens/Weather/WeatherSearchCity.tsx
+++ src/screens/Weather/WeatherSearchCity.tsx
@@ -11,6 +11,7 @@ import { getLatitude, getLongitude } from '@utils/utils'
 import React from 'react'
 import { ActivityIndicator, StatusBar, Text, TouchableOpacity, View } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
+import type { ViewProps } from 'react-native-svg/lib/typescript/fabric/utils'
 import { searchCity, type WeatherSearchResult } from './api'
 
 type ParamList = {
@@ -28,82 +29,92 @@ export default function WeatherSearchCity({ navigation, route }: { navigation: S
     mutationKey: ['cities'],
     mutationFn: () => searchCity(query.trim()),
     onError: (err) => console.log(err),
-    onSuccess: (data) => computeEverything(),
+    onSuccess: (_) => computeEverything(),
   })
 
+  const isData = data && data.length > 0
+
   return (
-    <>
+    <View className='flex-1 dark:bg-zinc-950'>
       <StatusBar barStyle='default' />
 
-      <View className='p-5 pb-2 pt-4 dark:bg-zinc-950' style={{ gap: 12 }}>
+      <View className='p-5 pb-2 pt-4' style={{ gap: 12 }}>
         <StackHeader title='Search City' navigation={navigation} left='Cancel' right='Done' />
         <Search placeholder='Search City' keyboardType='web-search' value={query} onChangeText={setQuery} autoFocus onEndEditing={() => mutate()} />
       </View>
 
-      <View className='dark:bg-zinc-950'>
-        {isPending && <Pending />}
-        {error && <ErrorFetchingCities error={error} navigation={navigation} />}
-        {data && data.length === 0 && <NoCityFound />}
-        {!isPending && !data && !error && <InitialMessage />}
-      </View>
+      {isPending && <Pending />}
+      {error && <ErrorFetchingCities error={error} navigation={navigation} />}
+      {data && data.length === 0 && <NoCityFound />}
+      {!isPending && !data && !error && <InitialMessage />}
 
-      <FlatList
-        className='px-5 dark:bg-zinc-950'
-        contentContainerStyle={{ gap: 3, paddingBottom: 50 }}
-        ListFooterComponent={data && data.length > 0 && <PoweredByAccuWeather />}
-        data={data}
-        keyExtractor={(item) => item.Key}
-        renderItem={({ item }) => <CityCard item={item} navigation={navigation} shouldGoBack={route.params.shouldGoBack} />}
-      ></FlatList>
-    </>
+      {isData && (
+        <FlatList
+          className='px-5'
+          contentContainerStyle={{ gap: 3, paddingBottom: 50, paddingTop: 5 }}
+          ListFooterComponent={data && data.length > 0 && <PoweredByAccuWeather />}
+          data={data}
+          keyExtractor={(item) => item.Key}
+          renderItem={({ item }) => <CityCard item={item} navigation={navigation} shouldGoBack={route.params.shouldGoBack} />}
+        />
+      )}
+    </View>
+  )
+}
+
+function MyFlex({ children, style, ...rest }: ViewProps) {
+  return (
+    <View className='flex-1 items-center justify-center px-10 pb-[35%]' style={[{ gap: 10 }, style]} {...rest}>
+      {children}
+    </View>
   )
 }
 
 function Pending() {
   return (
-    <View className='p-10 py-20' style={{ gap: 10 }}>
+    <MyFlex>
       <ActivityIndicator size='large' color={Colors.accent} />
       <Text style={PoppinsMedium} className='mt-2 text-center text-xs text-zinc-500 dark:text-zinc-500'>
         Searching for cities...
       </Text>
-    </View>
+    </MyFlex>
   )
 }
 
 function ErrorFetchingCities({ error, navigation }: { error: any; navigation: StackNav }) {
   return (
-    <View className='p-10 py-20' style={{ gap: 10 }}>
-      <Text style={PoppinsSemiBold} className='text-center text-2xl text-zinc-700 dark:text-zinc-300'>
+    <MyFlex>
+      <Text style={PoppinsSemiBold} className='text-center text-2xl text-zinc-800 dark:text-zinc-200'>
         {error?.message || 'Failed to fetch cities'}
       </Text>
       <Text style={PoppinsMedium} className='text-center text-sm text-zinc-500 dark:text-zinc-500'>
         This error may occur if you are not connected to the internet or API limit exceeded. To fix this, enter a API key in settings.
       </Text>
-      <BtnTransparent className='text-sm' onPress={() => navigation.navigate('WeatherSettings')} title='Set API key'></BtnTransparent>
-    </View>
+      <BtnTransparent className='text-sm' onPress={() => navigation.navigate('WeatherSettings')} title='Set API keys'></BtnTransparent>
+    </MyFlex>
   )
 }
 
 function NoCityFound() {
   return (
-    <View className='p-10 py-20' style={{ gap: 10 }}>
-      <Text style={PoppinsSemiBold} className='text-center text-2xl text-zinc-700 dark:text-zinc-300'>
+    <MyFlex>
+      <Text style={PoppinsSemiBold} className='text-center text-2xl text-zinc-800 dark:text-zinc-200'>
         No cities found
       </Text>
       <Text style={PoppinsMedium} className='text-center text-sm text-zinc-500 dark:text-zinc-500'>
         No cities found for the search query. Try searching with different keywords.
       </Text>
-    </View>
+    </MyFlex>
   )
 }
 
 function InitialMessage() {
   return (
-    <View className='p-10 py-20' style={{ gap: 10 }}>
-      <Text style={PoppinsMedium} className='text-center text-xs text-zinc-500 dark:text-zinc-500'>
+    <MyFlex>
+      <Text style={PoppinsMedium} className='text-center text-sm text-zinc-500 dark:text-zinc-500'>
         Enter a city or location name
       </Text>
-    </View>
+    </MyFlex>
   )
 }
 
@@ -122,7 +133,7 @@ function CityCard({ item, navigation, shouldGoBack }: { item: WeatherSearchResul
   const lon = getLongitude(item.GeoPosition.Longitude)
   return (
     <TouchableOpacity
-      className='flex-row justify-between rounded-2xl p-3.5 px-2'
+      className='w-full flex-row justify-between rounded-2xl p-3.5 px-2'
       activeOpacity={0.7}
       onPress={() => {
         navigation.navigate('ConfirmCity', {
@@ -146,10 +157,10 @@ function CityCard({ item, navigation, shouldGoBack }: { item: WeatherSearchResul
       </View>
 
       <View>
-        <Text className='text-right text-accent' style={[PoppinsMedium, { fontSize: 13 }]}>
+        <Text className='text-right text-accent' style={[PoppinsMedium, { fontSize: 12 }]}>
           {lat}, {lon}
         </Text>
-        <Text className='text-right text-zinc-600 dark:text-zinc-400' style={[PoppinsMedium, { fontSize: 13 }]}>
+        <Text className='text-right text-accent' style={[PoppinsMedium, { fontSize: 12 }]}>
           Altitude: {item.GeoPosition.Elevation.Metric.Value} {item.GeoPosition.Elevation.Metric.Unit}
         </Text>
       </View>
