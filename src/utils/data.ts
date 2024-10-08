bd0329b5bf18ef9d8ac6f01f7a2029d48f309db1
Abinash Karmakar <codeAbinash@gmail.com>
2024-10-09 00:11:16 +0530

    Update app version to 1.0.1 and version code to 2

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,7 +1,7 @@
 import { Linking } from 'react-native'
 
-export const APP_VERSION = '1.0.0'
-export const APP_VERSION_CODE = 46
+export const APP_VERSION = '1.0.1'
+export const APP_VERSION_CODE = 2
 export const ASK_QN_URL = 'https://t.me/+u6qGi8wPRmAxOGZl'
 export const APP_TG_CHANNEL = 'tg://resolve?domain=tech_triangle'
 export function ask_a_question() {
