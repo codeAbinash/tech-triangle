87893a8e1942cfeee11868e1e3efa598b4068054
Abinash Karmakar <codeAbinash@gmail.com>
2024-10-22 21:21:03 +0530

    Refactor About component to use react-query for fetching version data

--- src/screens/Settings/Extra/About.tsx
+++ src/screens/Settings/Extra/About.tsx
@@ -1,31 +1,26 @@
 import AppIconRound from '@assets/icons/icon_round.svg'
+import { useQuery } from '@tanstack/react-query'
 import { client } from '@utils/client'
 // import { client } from '@utils/client'
 import { APP_VERSION } from '@utils/data'
 import { SemiBold } from '@utils/fonts'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { View } from 'react-native'
 
 export default function About() {
-  const [version, setVersion] = useState('')
-
-  async function loadVersion() {
-    const res = await client.api.version.$get()
-    const v = await res.json()
-    setVersion(v.data?.version)
-    console.log(v.data?.version)
-    // Device remove
-  }
-  useEffect(() => {
-    loadVersion()
-  }, [])
+  const { data, isPending } = useQuery({
+    queryKey: ['version'],
+    queryFn: async () => (await client.api.version.$get()).json(),
+  })
 
   return (
     <View className='flex-1 items-center justify-center'>
       <AppIconUI />
       <SemiBold className='text-base text-black dark:text-white'>Tech Triangle</SemiBold>
-      <SemiBold className='text-xs text-black/50 dark:text-white'>v{APP_VERSION}</SemiBold>
-      <SemiBold className='text-xs text-black/50 dark:text-white'>v{version}</SemiBold>
+      <SemiBold className='text-xs text-black/50 dark:text-white'>App Version: v{APP_VERSION}</SemiBold>
+      <SemiBold className='text-xs text-black/50 dark:text-white'>
+        Current Version: v{isPending ? 'Loading...' : data?.data?.version}
+      </SemiBold>
     </View>
   )
 }
