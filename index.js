5276496303a44c07f68f56da08f3979c1fe9a381
Abinash Karmakar <codeAbinash@gmail.com>
2024-12-02 00:04:39 +0530

    Refactor build workflow to simplify APK handling and update architecture settings; remove unused artifact uploads and global type definitions

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { AppRegistry } from 'react-native'
 import App from './App'
 import { name as appName } from './app.json'
 import { Text, TextInput } from 'react-native'
+import 'react-native-gesture-handler'
 
 Text.defaultProps = Text.defaultProps || {}
 Text.defaultProps.allowFontScaling = false
