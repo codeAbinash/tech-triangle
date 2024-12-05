80fd67d09a13fb013e900df8447b26f2312c2153
Abinash Karmakar <codeAbinash@gmail.com>
2024-12-06 01:08:58 +0530

    Configure Reanimated logger to set warning level and disable strict mode

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ import App from './App'
 import { name as appName } from './app.json'
 import { Text, TextInput } from 'react-native'
 import 'react-native-gesture-handler'
+import { configureReanimatedLogger, ReanimatedLogLevel } from 'react-native-reanimated'
+
+configureReanimatedLogger({
+  level: ReanimatedLogLevel.warn,
+  strict: false, // Reanimated runs in strict mode by default
+})
 
 Text.defaultProps = Text.defaultProps || {}
 Text.defaultProps.allowFontScaling = false
